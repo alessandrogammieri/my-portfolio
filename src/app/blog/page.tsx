0b8519e4bb3d6090967dfa1ead40f4ea57179dfa
alessandrogammieri <alessandrogammieri.it@gmail.com>
2025-08-11 17:10:45 +0200

    feat: add pages About, Blog, Contact and Work

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,23 @@
+export default function Blog() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl font-bold">Blog Page</h1>
+        <p className="text-lg">
+          This is the Blog page of our application. Here you can find various
+          tasks and projects.
+        </p>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Next.js
+        </a>
+      </footer>
+    </div>
+  );
+}
