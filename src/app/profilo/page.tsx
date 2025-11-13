2065051f2dac213a57e6b2c4fc33c4ca93788c79
alessandrogammieri <alessandrogammieri.it@gmail.com>
2025-11-13 10:14:54 +0100

    feat: add new web app manifest and icons for portfolio site

--- src/app/profilo/page.tsx
+++ src/app/profilo/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
   return (
     <div className="min-h-[calc(100vh-10.5rem)] md:min-h-[calc(100vh-9rem)] min-w-0 w-full flex justify-center relative p-3 md:p-6 lg:p-10">
       <main className="min-h-0 min-w-0 w-full flex justify-center relative">
-        <div className="max-w-5xl w-full flex flex-col relative">
+        <div className="max-w-5xl w-full flex flex-col relative pt-7 pb-3">
           <ProfileNavigation />
           <div className="min-w-0 w-full flex flex-col md:flex-row justify-center relative">
             <ProfilePicture />
